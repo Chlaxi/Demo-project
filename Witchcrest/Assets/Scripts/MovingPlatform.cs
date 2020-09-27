0b89ae29b93ee2c23c048d373891a7866275d317
Chlaxi <chlaxi.lunari@gmail.com>
2020-09-27 22:31:40 +0200

    Moving platforms

---
+++ Witchcrest/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    Transform other;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            collision.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            collision.transform.SetParent(null);
+    }
+}
