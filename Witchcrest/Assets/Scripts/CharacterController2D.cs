0b30abad8eb99b8a111aa7d44748d5834d35c33a
Chlaxi <chlaxi.lunari@gmail.com>
2020-09-30 21:28:16 +0200

    Bug fixing
    When climbing ladders, it wouldn't always let you through platforms.

--- Witchcrest/Assets/Scripts/CharacterController2D.cs
+++ Witchcrest/Assets/Scripts/CharacterController2D.cs
@@ -98,6 +98,7 @@ public class CharacterController2D : MonoBehaviour
 		if (IsClimbing())
 		{
 			isClimbing = false;
+			StartCoroutine("PassThroughFloor");
 		}
 
 		if (IsGroundPassThrough() && isCrouching)
